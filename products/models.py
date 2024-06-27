de33540691fb46b943888123e7f1efa22e3ec513
Marc Hopkins <marchopkins1@outlook.com>
2024-06-27 15:21:59 +0000

    Update product params in product view

--- products/models.py
+++ products/models.py
@@ -13,7 +13,6 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField(blank=True, null=True)
