202205916f76594c9dad25051e9c3f1eb4d642c9
Marc Hopkins <marchopkins1@outlook.com>
2024-07-10 13:48:31 +0000

    Alter checkout model

--- checkout/models.py
+++ checkout/models.py
@@ -60,7 +60,7 @@ class OrderLineItem(models.Model):
     product_size = models.CharField(max_length=2, null=True, blank=True) 
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
-    product_variant = models.ForeignKey(ProductVariant, null=False, blank=False, on_delete=models.CASCADE)
+    product_variant = models.ForeignKey(ProductVariant, null=True, blank=True, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
 
