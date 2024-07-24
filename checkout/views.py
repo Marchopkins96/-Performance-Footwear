a6ce4ac1d3a306d579293e963109c04baf02fb8f
Marc Hopkins <marchopkins1@outlook.com>
2024-07-24 15:40:42 +0000

    Update checkout model

--- checkout/views.py
+++ checkout/views.py
@@ -70,6 +70,7 @@ def checkout(request):
                         order_line_item.save()
                     else:
                         for size, quantity in item_data['items_by_size'].items():
+                            print(f'{size}, {quantity}')
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
