fbd6531ec6a86b9801384e439ebbcf1f34955658
Marc Hopkins <marchopkins1@outlook.com>
2024-07-25 15:15:27 +0000

    Add wishlist icon to products

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Wishlist
 
-# Register your models here.
+class WishlistAdmin(admin.ModelAdmin):
+
+    search_fields = [
+        "user",
+    ]
+
+    filter_horizontal = ("products",)
+    list_display = (
+        "user",
+        "date_added",
+    )
+
+    ordering = ("user",)
+
+
+admin.site.register(Wishlist, WishlistAdmin)
