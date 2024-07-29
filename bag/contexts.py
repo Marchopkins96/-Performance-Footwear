0ba13382d836758f69ccb2ce0a3b6d5ee6aea45a
Marc Hopkins <marchopkins1@outlook.com>
2024-07-29 15:43:18 +0000

    Add sale price to bag page

--- bag/contexts.py
+++ bag/contexts.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
+from django.contrib import messages
 
 def bag_contents(request):
 
@@ -13,23 +14,33 @@ def bag_contents(request):
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
+            if product.on_sale:
+                price = product.sale_price
+            else:
+                price = product.price
+            total += item_data * price
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
+                "price": price,
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
+                if product.on_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                total += quantity * price
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
                     'product': product,
                     'size': size,
+                    "price": price,
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
