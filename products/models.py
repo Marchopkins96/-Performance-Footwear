985438e0abea2c0a52e8bf931b6e84f02a772a34
Marc Hopkins <marchopkins1@outlook.com>
2024-07-29 10:27:32 +0000

    Add Review class in products model

--- products/models.py
+++ products/models.py
@@ -104,3 +104,21 @@ class Brand(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
+
+class Reviews(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=1500, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user", "product"], name="reviews_per_user")
+        ]
+
+    def __str__(self):
+        return self.title
