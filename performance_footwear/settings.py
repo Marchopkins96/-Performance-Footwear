7df055add999efd7a1a2eeb586f45b715f9b98d7
Marc Hopkins <marchopkins1@outlook.com>
2024-08-02 14:24:26 +0000

    remove typo

--- performance_footwear/settings.py
+++ performance_footwear/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-# DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-marchopkins-performance-r6zs2ybvt6o.ws.codeinstitute-ide.net', 'performance-footwear-ed28bb401f1b.herokuapp.com',]
 
